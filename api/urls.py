d26486371fc02ae50f79c9f76fb570c89617ffb6
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-08-01 00:16:53 -0300

    add swagger and openapi scheme

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.schemas import get_schema_view
 
 from .views import SubjectViewSet
 
@@ -14,6 +17,15 @@ router.register('subjects', SubjectViewSet)
 urlpatterns = [
     path('auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things...",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('docs/swagger/', TemplateView.as_view(
+        template_name = 'swagger-ui.html',
+        extra_context = {'schema_url' : 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
 if settings.DEBUG:
     urlpatterns += [
