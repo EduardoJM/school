2730e8aa749282fbdaaab7be4bbbdef75b512286
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-08-28 11:12:21 -0300

    feat(admin): update the resources images changelist page

--- packages/core/admin/resources/admin.py
+++ packages/core/admin/resources/admin.py
@@ -8,6 +8,10 @@ from .forms import QuestionImageForm
 class QuestionImageAdmin(admin.ModelAdmin):
     fields = ['title', 'image']
     form = QuestionImageForm
+    change_list_template = 'admin/change_list_images.html'
+    list_display = ['display_image']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['title']
 
     def get_actions(self, request):
         return {}
--- packages/core/admin/resources/models.py
+++ packages/core/admin/resources/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.html import format_html
 from core.models import TimestampsMixin
 
 class QuestionImage(TimestampsMixin, models.Model):
@@ -12,6 +12,10 @@ class QuestionImage(TimestampsMixin, models.Model):
         verbose_name = 'Título',
         max_length = 100,
     )
+
+    def display_image(self):
+        return format_html('<div class="img" style="background-image: url(%s)" title="%s"></div>' % (self.image.url, self.title))
+    display_image.short_description = 'Imagem'
 
     def __str__(self):
         return self.title
