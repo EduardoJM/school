f1c967c3e4ead7ba873594a3a9d27dd974fd0614
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-09-25 11:42:45 -0300

    feat(admin): add admin backdrop when loading auth informations

--- packages/core/admin/src/contexts/auth.tsx
+++ packages/core/admin/src/contexts/auth.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import { Backdrop, CircularProgress } from '@mui/material';
 import { useSnackbar } from 'notistack';
 import { User } from '../entities';
 import { api } from '../services/api';
@@ -16,17 +17,21 @@ export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 export const AuthProvider: React.FC = ({ children }) => {
     const { enqueueSnackbar } = useSnackbar();
+    const [loading, setLoading] = useState(false);
     const [user, setUser] = useState<User | null>(null);
 
     async function handleLogin(email: string, password: string) {
         try {
+            setLoading(true);
             const userResponse = await login(email, password);
             setUser(userResponse.user);
 
             api.defaults.headers.Authorization = `Bearer ${userResponse.token}`;
 
             localStorage.setItem('@INVENTARE:JWT', userResponse.token);
+            setLoading(false);
         } catch(err) {
+            setLoading(false);
             enqueueSnackbar(getDisplayErrorMessage(err), { variant: 'error' });
         }
     }
@@ -43,15 +48,18 @@ export const AuthProvider: React.FC = ({ children }) => {
             if (!jwt) {
                 return;
             }
+            setLoading(true);
             api.defaults.headers.Authorization = `Bearer ${jwt}`;
 
             try {
                 const user = await validate();
                 setUser(user);
+                setLoading(false);
             } catch(err) {
                 enqueueSnackbar(getDisplayErrorMessage(err), { variant: 'error' });
                 api.defaults.headers.Authorization = undefined;
                 localStorage.removeItem('@INVENTARE:JWT');
+                setLoading(false);
             }
         }
 
@@ -59,16 +67,24 @@ export const AuthProvider: React.FC = ({ children }) => {
     }, [enqueueSnackbar]);
 
     return (
-        <AuthContext.Provider
-            value={{
-                loggedIn: !!user,
-                user,
-                login: handleLogin,
-                logout: handleLogout,
-            }}
-        >
-            { children }
-        </AuthContext.Provider>
+        <>
+            <AuthContext.Provider
+                value={{
+                    loggedIn: !!user,
+                    user,
+                    login: handleLogin,
+                    logout: handleLogout,
+                }}
+            >
+                { children }
+            </AuthContext.Provider>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={loading}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        </>
     );
 };
 
