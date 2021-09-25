e3daebffcc490b13d9f2153f7abf0f48e0676d44
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-09-24 23:16:07 -0300

    feat(admin): add userType to admin login

--- packages/core/admin/src/services/peoples/auth/index.ts
+++ packages/core/admin/src/services/peoples/auth/index.ts
@@ -2,6 +2,6 @@ import { api } from '../../api';
 import { UserAuth } from '../../../entities';
 
 export async function login(email: string, password: string): Promise<UserAuth> {
-    const result = await api.post<UserAuth>('/peoples/auth', { email, password });
+    const result = await api.post<UserAuth>('/peoples/auth', { email, password, userTypes: ['ADMIN', 'TEACHER'] });
     return result.data;
 }
