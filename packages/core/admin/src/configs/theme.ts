ed80db9c9ef0405228fc9a6c21387831a92a7da5
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-10-10 18:11:48 -0300

    fix(admin): change theme base to light

--- packages/core/admin/src/configs/theme.ts
+++ packages/core/admin/src/configs/theme.ts
@@ -10,7 +10,7 @@ export const primaryColor = {
 
 export const theme = createTheme({
     palette: {
-        mode: 'dark',
+        mode: 'light',
         primary: primaryColor,
     },
     components: {
