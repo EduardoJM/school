a20ca6a5b29db1a58cda5b4e2ba5b66fa26cddad
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-10-16 21:49:13 -0300

    refactor(sdk): move error getMessage util to sdk

--- packages/shared/sdk/index.ts
+++ packages/shared/sdk/index.ts
@@ -1,3 +1,4 @@
 export * from './api';
 export * from './entities';
-export * from './services'
+export * from './services';
+export * from './utils';
---
+++ packages/shared/sdk/utils/errorsUtils.ts
@@ -0,0 +1,17 @@
+export const errorUtils = {
+    getDisplayErrorMessage(err: any, defaultMessage: string = 'Houve um erro desconhecido!') {
+        if (!err) {
+            return defaultMessage;
+        }
+        if (!err.response) {
+            return defaultMessage;
+        }
+        if (!err.response.data) {
+            return defaultMessage;
+        }
+        if (!err.response.data.message) {
+            return defaultMessage;
+        }
+        return err.response.data.message;
+    },
+};
