fe7cdd46cce17955f714117d9bbe72a67a9a5d06
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-10-30 23:44:07 -0300

    test(api): add first tests for admin entity

---
+++ packages/core/api/src/components/peoples/admin/__tests__/AdminEntity.spec.ts
@@ -0,0 +1,17 @@
+import { User } from '../../user/UserEntity';
+import { Admin } from '../AdminEntity';
+
+describe('Admin Entity', () => {
+    it('The serialize must be return a type and the parent user properties', async () => {
+        const user = User.create({ email: 'email@email.com', fullName: 'Email', useGravatar: false });
+        const admin = new Admin();
+        admin.user = user;
+        user.admin = admin;
+
+        const data1 = await user.serialize();
+        const data2 = await admin.serialize();
+        expect(data1).toHaveProperty('type');
+        expect(data1.type).toBe('ADMIN');
+        expect(data1).toMatchObject(data2);
+    });
+});
