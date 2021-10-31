671b285f5cf05b75acee110b66e8ac680cd401ad
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-10-30 23:44:27 -0300

    test(api): add basic tests for Student entity

---
+++ packages/core/api/src/components/peoples/student/__tests__/StudentEntity.spec.ts
@@ -0,0 +1,17 @@
+import { User } from '../../user/UserEntity';
+import { Student } from '../StudentEntity';
+
+describe('Student Entity', () => {
+    it('The serialize must be return a type and the parent user properties', async () => {
+        const user = User.create({ email: 'email@email.com', fullName: 'Email', useGravatar: false });
+        const student = new Student();
+        student.user = user;
+        user.student = student;
+
+        const data1 = await user.serialize();
+        const data2 = await student.serialize();
+        expect(data1).toHaveProperty('type');
+        expect(data1.type).toBe('STUDENT');
+        expect(data1).toMatchObject(data2);
+    });
+});
